4b2a3c5cc937a3ebf841600796c559537a987a22
Raphael Levy <raffalevy@me.com>
2014-03-26 09:17:43 -0400

    Allominecy v0.3.5
    Range of iron fixed

---
+++ src/src/com/raphaellevy/allominecy/metals/Tin.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.raphaellevy.allominecy.metals;
+
+import com.raphaellevy.allominecy.Allominecy;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+/**
+ *
+ * @author raffa
+ */
+public class Tin extends StackedMetal{
+    public static void burn(Player play,ItemStack soap,Allominecy plug) {
+        if(plug.getConfig().getBoolean("players."+ play.getPlayerListName() + ".tin")) {
+            play.setMetadata("burning",new FixedMetadataValue(plug,true));
+
+            play.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION,100,1),true);
+        }
+        UseUp(soap,play);
+        
+    }
+}
